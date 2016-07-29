5717dfbac176bae99a118212fa4870dbd4b92052
Matteo <matteo@boostco.de>
2016-07-29 23:22:38 +0200

    more verbose comments

--- StoryboardEnum/Classes/StoryboardEnum.swift
+++ StoryboardEnum/Classes/StoryboardEnum.swift
@@ -8,35 +8,47 @@
 
 import UIKit
 
+// MARK: - StoryboardVC
 public struct StoryboardVC {
     
+    // reference to current window
     public var window: UIWindow?
     
+    // initter with window, this allow us to avoid to pass always the window
     public init(inWindow window: UIWindow?){
         self.window = window
     }
     
+    // retrieves the vc from the storyboard with an identifier
     public func getVC(fromStoryboard name: String, withIdentifier identifier: String? = nil) -> UIViewController? {
         
+        // retrieve storyboard according name
         let storyboard = UIStoryboard(name: name, bundle: NSBundle.mainBundle())
         
+        // if an idientifier isset ask for that 
         if let idx = identifier {
+            // return it
             return storyboard.instantiateViewControllerWithIdentifier(idx)
         }
-        
+        // otherwise initial view controller is used
         return storyboard.instantiateInitialViewController()
     }
     
 }
 
+// MARK: - Protocol
 public protocol StoryboardEnum {
     var vc: UIViewController? { get }
     var story: StoryboardVC { get }
 }
 
+// MARK: - Extension
 extension StoryboardEnum {
+    // show the current vc
     public func show() {
+        // try to retrieve current window
         guard let window = story.window else { return }
+        // set vc as rootVc
         window.rootViewController = vc
         window.makeKeyAndVisible()
     }
